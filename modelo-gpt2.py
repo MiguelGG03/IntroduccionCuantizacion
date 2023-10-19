3c81a53ce6c2dad3ed7ba21bbae44ce259fcc044
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2023-10-19 18:21:15 +0200

    UWU

--- modelo-gpt2.py
+++ modelo-gpt2.py
@@ -1,12 +1,17 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-model_name = "gpt2"  # Nombre del modelo preentrenado (puedes cambiarlo según tus necesidades)
+# Cargar el modelo preentrenado GPT-2
+model_name = "gpt2"
 model = GPT2LMHeadModel.from_pretrained(model_name)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+#Limpiar terminal
+print("\033c")
 
-input_text = input("Ingrese el texto:")
-input_ids = tokenizer.encode(input_text, return_tensors="pt")
+# Generar texto de muestra
+prompt = "En un mundo lejano"
+input_ids = tokenizer.encode(prompt, return_tensors="pt")
 output = model.generate(input_ids, max_length=50, num_return_sequences=1, no_repeat_ngram_size=2, top_k=50)
 
-generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-print(generated_text)
+for sequence in output:
+    generated_text = tokenizer.decode(sequence, skip_special_tokens=True)
+    print("Texto Generado:", generated_text)
